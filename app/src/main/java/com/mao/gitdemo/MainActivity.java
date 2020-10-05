927a270b4063786796c58a9a9a70bb60fb160782
maoweiyi <maoweiyi@hztianque.com>
2020-10-05 20:11:42 +0800

    add 3 notes

--- app/src/main/java/com/mao/gitdemo/MainActivity.java
+++ app/src/main/java/com/mao/gitdemo/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //注释1
+        //注释2
+        //注释3
     }
 }
